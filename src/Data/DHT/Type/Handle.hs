665fa314a08b35bd091ba143462603d171f87ea8
Peter Trško <peter.trsko@gmail.com>
2015-08-29 23:06:47 +0200

    instance Show DhtHandle; function withDhtHandle
    Mostly for debugging purposes we need to be able to print some basic
    information about DhtHandle. It is up to DHT implementation to provide
    enough insight using Show instance of its state. In example it can
    contain things like DHT instance name and DHT node ID.
    
    Function withDhtHandle simplifies definition of functions that need
    access to implementation specific data wrapped inside DhtHandle. Its
    type signature is:
    
        withDhtHandle
            :: (forall s. Show s => DhtHandle' s -> s -> a)
    	-> DhtHandle
    	-> a

--- src/Data/DHT/Type/Handle.hs
+++ src/Data/DHT/Type/Handle.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RankNTypes #-}
 -- |
 -- Module:       $HEADER$
 -- Description:  Abstract API for DHT implementations.
@@ -10,7 +11,7 @@
 --
 -- Stability:    experimental
 -- Portability:  DeriveDataTypeable, DeriveGeneric, NoImplicitPrelude,
---               ExistentialQuantification, NoImplicitPrelude
+--               ExistentialQuantification, NoImplicitPrelude, RankNTypes
 --
 -- Abstract API for DHT implementations.
 module Data.DHT.Type.Handle
@@ -18,8 +19,13 @@ module Data.DHT.Type.Handle
 --    )
     where
 
+import Prelude (Num((+)))
+
+import Data.Function ((.), ($))
+import Data.Ord (Ord((>)))
 import Data.Typeable (Typeable)
 import GHC.Generics (Generic)
+import Text.Show (Show(showsPrec), showParen, showString)
 
 import Data.DHT.Type.Key (DhtKey)
 import Data.DHT.Type.Result (DhtResult)
@@ -27,7 +33,20 @@ import Data.DHT.Type.Result (DhtResult)
 
 data DhtValue
 
-data DhtHandle = forall s. DhtHandle (DhtHandle' s)
+-- | Abstract DHT handle of existing DHT instance.
+--
+-- Mostly for debugging purposes we need to be able to print some basic
+-- information about 'DhtHandle'. It is up to DHT implementation to provide
+-- enough insight using 'Show' instance of its state. In example it can contain
+-- things like DHT instance name and DHT node ID.
+data DhtHandle = forall s. Show s => DhtHandle (DhtHandle' s)
+  deriving (Typeable)
+
+instance Show DhtHandle where
+    showsPrec n = withDhtHandle $ \_ s -> showParen (n > applicationPrecedence)
+        $ showString "DhtHandle " . showsPrec (applicationPrecedence + 1) s
+      where
+        applicationPrecedence = 10
 
 -- | Low-level DHT handle that is used by DHT implementations, but not consumers.
 data DhtHandle' s = DhtHandle'
@@ -38,3 +57,12 @@ data DhtHandle' s = DhtHandle'
     , insert :: s -> DhtKey -> DhtValue -> DhtResult ()
     }
   deriving (Generic, Typeable)
+
+-- | Helper function that simplifies unwrapping 'DhtHandle'.
+withDhtHandle
+    :: (forall s. Show s => DhtHandle' s -> s -> a)
+    -- ^ Function that needs access to implementation specific 'DhtHandle''.
+    -- 'Show' is the only thing we are allowed to know about internal state.
+    -> DhtHandle
+    -> a
+withDhtHandle f (DhtHandle h@DhtHandle'{state = s}) = f h s
