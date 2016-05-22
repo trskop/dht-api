3ccd8c22b74329ada680eb228c55e37d0d3fd9f6
Peter Trško <peter.trsko@gmail.com>
2016-05-22 16:07:01 +0200

    Removed unused language extension

--- src/Data/DHT/Type/Hash.hs
+++ src/Data/DHT/Type/Hash.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 -- |
