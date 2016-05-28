2a72fdccd8b8484e706cbbe4a4ed92f73dfda0ae
Peter Trško <peter.trsko@gmail.com>
2016-05-28 19:12:44 +0200

    Special DhtKey constructor for Text values
    Users can not assume what text encoding is used by the DHT
    implementation.
    
    Similar thing goes for other values, e.g. numbers (big endian vs. little
    endian vs. mixed endian, various floating point encodings, etc.), but
    that is not handled at the moment.

--- src/Data/DHT/Type/Key.hs
+++ src/Data/DHT/Type/Key.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 -- |
 -- Module:       $HEADER$
 -- Description:  Data type for keys used in DHT.
--- Copyright:    (c) 2015 Jan Šipr, Matej Kollár, Peter Trško
+-- Copyright:    (c) 2015 Jan Šipr, Matej Kollár; 2015-2016 Peter Trško
 -- License:      BSD3
 --
 -- Stability:    experimental
@@ -13,7 +14,6 @@
 -- Data type for keys used in DHT.
 module Data.DHT.Type.Key
     ( DhtKey(..)
-    , toByteString
     )
   where
 
@@ -23,25 +23,31 @@ import Data.Function ((.))
 import Data.Ord (Ord)
 import Data.String (IsString(fromString))
 import GHC.Generics (Generic)
-import Text.Show (Show(showsPrec))
+import Text.Show (Show(showsPrec), showString)
 
 import Data.ByteString (ByteString)
 import Data.Hashable (Hashable)
+import Data.Text (Text)
 
 
 -- | DHT is a map from keys to values, this data type represents the keys
 -- accepted by DHT implementation.
-newtype DhtKey = DhtKey ByteString
+data DhtKey
+    = DhtKeyText !Text
+    -- ^ Text is treated differently, since its up to a specific hash
+    -- implementation to interpret text encoding correctly. In example it may
+    -- treat 'Text' as UTF-8 encoded string of bytes.
+    | DhtKey !ByteString
   deriving (Data, Eq, Generic, Ord, Typeable)
 
 instance Show DhtKey where
-    showsPrec n (DhtKey bs) = showsPrec n bs
+    showsPrec _ k = showString "<<DhtKey " . showContent k . showString ">>"
+      where
+        showContent = \case
+            DhtKeyText t -> showsPrec 0 t
+            DhtKey bs    -> showsPrec 0 bs
 
 instance IsString DhtKey where
-    fromString = DhtKey . fromString
+    fromString = DhtKeyText . fromString
 
 instance Hashable DhtKey -- Uses GHC Generics.
-
--- | Unpack 'DhtKey' in to (strict) 'ByteString'.
-toByteString :: DhtKey -> ByteString
-toByteString (DhtKey bs) = bs
